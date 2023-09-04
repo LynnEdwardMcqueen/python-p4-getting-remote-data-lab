36fe2dff4df72b1b037f7866fad1c0f482f35e26
Lynn McQueen <lynn.edward.mcqueen@gmail.com>
2023-09-03 19:01:31 -0600

    Completes the test requirements

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,17 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+       
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return json.loads(response.content)
+    
+foo = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+boo = foo.get_response_body()
+print (boo)
+goo = foo.load_json()
+print (goo)
